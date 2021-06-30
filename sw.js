af0bee0dda98b50ee74a6c527600eb9c192719e9
xt <xt@xwat.com.cn>
2021-06-30 18:12:24 +0800

    恢复设置

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ const PRECACHE_LIST = [
   "./js/snackbar.js",
   "./img/icon_wechat.png",
   "./img/avatar-xtongs.jpg",
-  "./img/home-bg-o.jpg",
+  "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
