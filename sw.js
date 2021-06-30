e52c8a304b7251be10c9456ad32e3749d167ff1f
xt <xt@xwat.com.cn>
2021-06-30 17:50:10 +0800

    修改缓存图片

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ const PRECACHE_LIST = [
   "./js/bootstrap.min.js",
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
-  "./img/home-bg.jpg",
+  // "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
