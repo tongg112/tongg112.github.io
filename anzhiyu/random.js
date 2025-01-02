7fa408ff987b1a148b81d07cbe3fc709a469ea8d
xtongs <lotongok@gmail.com>
2025-01-02 23:54:17 +0800

    Site updated: 2025-01-02 23:54:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2021/06/30/centos下安装node环境/","2021/06/25/git常用命令速查/"];function toRandomPost(){
+var posts=["2025/01/02/Ubuntu下连接并使用Airpods/","2021/06/30/centos下安装node环境/","2021/06/25/git常用命令速查/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
